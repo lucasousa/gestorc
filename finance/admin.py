41fceefbbd09cb4f3ceb242e014a7bf5fa6457ed
samuel <samuellelis88@gmail.com>
2022-08-03 11:10:20 -0300

    register models on admin and ignoring asdf, ide files

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Address)
+admin.site.register(Company)
+admin.site.register(Invoice)
+admin.site.register(Contract)
