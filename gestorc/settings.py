cc0325ce60a7be2ed9c0b362694c591c6707ff19
Lucas Sousa <lsousa@worc.com.br>
2022-09-26 21:19:48 -0300

    add sqlite

--- gestorc/settings.py
+++ gestorc/settings.py
@@ -81,11 +81,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "gestorc.wsgi.application"
 
 
-from dj_database_url import parse as dburl
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-
-DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
-
+# from dj_database_url import parse as dburl
+# default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+#
+# DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
