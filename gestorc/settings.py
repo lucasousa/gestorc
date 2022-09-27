c011f6a5878175bdc5a4b3fc7336d98d0be2182c
Lucas Sousa <lsousa@worc.com.br>
2022-09-26 21:47:20 -0300

    add postgres

--- gestorc/settings.py
+++ gestorc/settings.py
@@ -81,16 +81,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "gestorc.wsgi.application"
 
 
-# from dj_database_url import parse as dburl
-# default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-#
-# DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from dj_database_url import parse as dburl
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
